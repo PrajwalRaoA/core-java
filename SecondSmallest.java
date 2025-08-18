9d402e8922bd988b8766c8f6abdebd874fef4451
PrajwalRaoA <121794604+PrajwalRaoA@users.noreply.github.com>
2025-08-18 10:08:49 +0530

    Add files via upload

---
+++ SecondSmallest.java
@@ -0,0 +1,19 @@
+class SecondSmallest{
+ public static void main(String[] args)
+{
+ int[] arr = {10,20,40,60,50};
+
+ int smallest = arr[0];
+ int secondSmallest = arr[1];
+
+ for(int i=0; i<arr.length; i++) {
+    if(arr[i] < smallest) {
+       secondSmallest = smallest;
+       smallest = arr[i];
+    } else if(arr[i] < secondSmallest && arr[i] != smallest) {
+       secondSmallest = arr[i];
+    }
+ }
+ System.out.println("Second smallest: " + secondSmallest);
+}
+}
